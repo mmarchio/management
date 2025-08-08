30e4b96aa0449febb4e66b7260d51f9489835f93
matt marchio <matt.marchio@gmail.com>
2025-08-07 18:37:24 -0700

    expanded videooutput

--- src/types/shallow_embed_videooutput.go
+++ src/types/shallow_embed_videooutput.go
@@ -3,13 +3,48 @@ package types
 import (
 	"context"
 	"encoding/json"
+
+	merrors "github.com/mmarchio/management/errors"
 )
 
 type ShallowVideoOutput struct {
-	EmbedModel
+	ShallowModel
 	ID 				VideoOutputID `json:"id"`
 	StatsModel 		string `json:"stats_model"`
 	FilesArrayModel []string `json:"files_model"`
+}
+
+func (c ShallowVideoOutput) Expand(ctx context.Context) (*VideoOutput, error) {
+	r := VideoOutput{}
+	if c.ShallowModel.CreatedAt.IsZero() && c.ShallowModel.ID != "" {
+		sc, err := c.ShallowModel.Get(ctx)
+		if err != nil {
+			return nil, merrors.ContentGetError{}.Wrap(err)
+		}
+		if err := json.Unmarshal([]byte(sc.Content), &r); err != nil {
+			return nil, merrors.JSONUnmarshallingError{}.Wrap(err)
+		}
+		return &r, nil
+	}
+	r.EmbedModel = r.EmbedModel.FromShallowModel(c.ShallowModel)
+	ss := ShallowStats{}
+	ss.ShallowModel.ID = c.StatsModel
+	stats, err := ss.Expand(ctx)
+	if err != nil {
+		return nil, merrors.ContentGetError{}.Wrap(err)
+	}
+	r.StatsModel = *stats
+	r.FilesArrayModel = make([]File, 0)
+	for _, id := range c.FilesArrayModel {
+		sf := ShallowFile{}
+		sf.ShallowModel.ID = id
+		f, err := sf.Expand(ctx)
+		if err != nil {
+			return nil, merrors.ContentGetError{}.Wrap(err)
+		}
+		r.FilesArrayModel = append(r.FilesArrayModel, *f)
+	}
+	return &r, nil
 }
 
 func (c *ShallowVideoOutput) Unmarshal(ctx context.Context, j string) error {
